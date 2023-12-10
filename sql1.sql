ff5bfc0e207e552af6d6c5739ef68ca3b5925564
shwetadarakh <153406987+shwetadarakh@users.noreply.github.com>
2023-12-10 08:26:02 +0530

    Create sql1.sql

---
+++ sql1.sql
@@ -0,0 +1,10 @@
+create table Employee_Detail
+(
+EmployeeId int not null,
+FirstName varchar(100),
+LastName varchar (100),
+Salary money,
+JoiningDate datetime,
+Department Varchar(50),
+Gender char(10)
+)
